5ec8d614987299dff2a653e7197dc134e4f27d73
Chehak Gupta <gchehak18@gmail.com>
2024-07-05 19:15:48 +0530

    Add files via upload

--- src/utils/appSlice.js
+++ src/utils/appSlice.js
@@ -4,13 +4,18 @@ const appSlice = createSlice({
   name: "app",
   initialState: {
     isMenuOpen: true,
+    videos : []
   },
   reducers: {
     toggleMenu: (state) => {
       state.isMenuOpen = !state.isMenuOpen;
     },
+    closeMenu :(state)=>{
+      state.isMenuOpen = false
+    }
+  
   },
 });
 
-export const { toggleMenu } = appSlice.actions;
+export const { toggleMenu,closeMenu  } = appSlice.actions;
 export default appSlice.reducer;
