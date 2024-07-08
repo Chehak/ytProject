e925c1d2cc84cb254060399bcc95aaa0f95c795b
Chehak Gupta <gchehak18@gmail.com>
2024-07-08 17:02:58 +0530

    Update WatchPage.js

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,22 +1,35 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { closeMenu } from '../utils/appSlice'
-import { useSearchParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
+import CommentsPage from "./CommentsPage";
 
 const WatchPage = () => {
+ 
   const dispatch = useDispatch();
   const [searchParam] = useSearchParams();
- const videoId = searchParam.get('v')
-  useEffect(()=>{
-    dispatch(closeMenu())
-  },[])
+  const videoId = searchParam.get("v");
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
   return (
-    <div>
-      
-      <iframe width="560" height="315" src={'https://www.youtube.com/embed/'+videoId} title="YouTube video player"
-       frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    <div className="px-5 flex flex-col">
+      <div>
+        <iframe
+          width="800"
+          height="400"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          src={
+            "https://www.youtube.com/embed/" + videoId + "?&autoplay=1&mute=1"
+          }
+          title="YouTube video player"
+        ></iframe>
+      </div>
+      <div>
+        <CommentsPage/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WatchPage
+export default WatchPage;
