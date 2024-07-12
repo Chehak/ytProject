7bffd9764d51420917dc39cef7adee50a9afa5d8
Chehak Gupta <gchehak18@gmail.com>
2024-07-12 19:09:45 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,14 +3,41 @@ import "./App.css";
 import Body from "./components/Body";
 import Header from "./components/Header";
 import store from "./utils/store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
+import SearchResults from "./components/SearchResults";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />,
+      },
+      {
+        path: "results",
+        element: <SearchResults />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <Provider store={store}>
-      <div>
+      <RouterProvider router={appRouter}>
         <Header />
-        <Body />
-      </div>
+        <div>
+          <Body />
+        </div>
+      </RouterProvider>
     </Provider>
   );
 }
